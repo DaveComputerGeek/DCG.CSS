cd0a195c0a3eb6f7bb70d7350bbe04c9de0b2d37
Dave Computer Geek <github@davecomputergeek.scot>
2020-07-09 17:45:26 +0100

    Updated Forms
    * Fixed issue where color overrides would not work, by removing default color from form input field.
    * Updated forms documentation with demonstrations and example code.

--- index.php
+++ index.php
@@ -15,12 +15,14 @@
 				<a class="dcg-nav-item dcg-lingrad-hover-alt-dark" href="/dcgcss"><b>DCG.CSS</b></a>
 				<a class="dcg-nav-item dcg-lingrad-hover-alt-dark" href="/dcgcss/typography">Typography</a>
 				<a class="dcg-nav-item dcg-lingrad-hover-alt-dark" href="/dcgcss/navigation">Navigation</a>
+				<a class="dcg-nav-item dcg-lingrad-hover-alt-dark" href="/dcgcss/forms">Forms</a>
 			</nav>
 			
 			<div class="dcg-padding">
 			<?php
 			switch( $page ) {
 				default:
+				case "home":
 					?>
 						<div class="dcg-xxxlarge dcg-center dcg-padding-top-xlarge"><b>Welcome to DCG.CSS!</b></div>
 						<div class="dcg-large dcg-center dcg-padding-bottom-xlarge">Your web project. Your way.</div>
@@ -52,7 +54,8 @@
 					<?php
 					break;
 				case "typography":
-					echo '<div class="dcg-xxlarge dcg-padding-bottom dcg-bold">Typogaphy</div>
+					?>
+					<div class="dcg-xxlarge dcg-padding-bottom dcg-bold">Typogaphy</div>
 						<div class="dcg-bold dcg-large dcg-padding-bottom">Text Sizes</div>
 						<div class="dcg-small">Small</div>
 						<div class="dcg-normal">Normal</div>
@@ -61,9 +64,11 @@
 						<div class="dcg-xxlarge">xxLarge</div>
 						<div class="dcg-xxxlarge">xxxLarge</div>
 						<div class="dcg-jumbo">Jumbo</div>';
+					<?php
 					break;
 				case "navigation":
-					echo '<div class="dcg-xxlarge"><b>Navigation</b></div>
+					?>
+					<div class="dcg-xxlarge"><b>Navigation</b></div>
 					<div class="dcg-bold dcg-large dcg-padding-bottom"><b>Basic Navigation Bar</b></div>
 					<div class="dcg-padding-bottom">The most basic navigation bar uses the <b>dcg-nav</b> and <b>dcg-nav-item</b> classes. This will create a horozontal list of links just like a bar across the page and by default will have a white background and black text. Here is a working example.</div>
 					<nav class="dcg-nav dcg-border dcg-border-all dcg-border-light-grey">
@@ -126,17 +131,72 @@
 	&lt;a class="dcg-nav-item" href="#"&gt;Item 2&lt;/a&gt;
 	&lt;a class="dcg-nav-item" href="#"&gt;Item 3&lt;/a&gt;
 &lt;/nav&gt;
-</pre>';
+</pre>
+					<?php
 					break;
 				case "forms":
-					echo '<form class="dcg-form">
-						<label for="name" class="dcg-small dcg-padding-top dcg-padding-bottom-none dcg-text-deep-red dcg-margin-none">Name:</label>
-						<input id="name" class="dcg-input dcg-border dcg-border-all dcg-border-deep-red dcg-margin-none">
-						
-						<select class="dcg-input">
-							<option>Test 1</option>
-						</select>
-					</form>';
+					?>
+					<div class="dcg-xxlarge"><b>Forms</b></div>
+					<div class="dcg-large dcg-padding-bottom"><b>Input Fields</b></div>
+					<div class="dcg-padding-bottom">Define a form using the <b>dcg-form</b> class, and inside that make an input field with the <b>dcg-input</b> class.<br><br><b>Demonstration with Example Code</b></div>
+					<form class="dcg-form">
+						<input class="dcg-input dcg-lighter-light-grey">
+					</form>
+<pre class="dcg-code">
+&lt;form class="dcg-form"&gt;
+	&lt;input class="dcg-input"&gt;
+&lt;/form&gt;
+</pre>
+					
+					<div class="dcg-padding-top dcg-padding-bottom dcg-large">Styled Input Fields</div>
+					<div class="dcg-padding-top dcg-padding-bottom"><b>Borders</b><br>The <b>dcg-input</b> class already applies the base of a border to the field, all it needs now is to know where to display it, what thickness, and what color.<br><br>Use any of the border color classes in combination with the border classes.<br><br><b>Demonstration of Deep Red Border with Example Code</b></div>
+					<form class="dcg-form">
+						<input class="dcg-input dcg-border-all dcg-border-deep-red dcg-text-deep-red">
+					</form>
+<pre class="dcg-code">
+&lt;form class="dcg-form"&gt;
+	&lt;input class="dcg-input dcg-border-all dcg-border-deep-red"&gt;
+&lt;/form&gt;
+</pre>
+					
+					<div class="dcg-padding-top dcg-padding-bottom">Borders do not need to be on all sides, you can specify just what sides you need. Let\'s apply a thick border to only the bottom to look like an underline.<br><br><b>Demonstration of Bottom Border with Example Code</b></div>
+					<form class="dcg-form">
+						<input class="dcg-input dcg-border-bottombar dcg-border-deep-red dcg-text-deep-red">
+					</form>
+<pre class="dcg-code">
+&lt;form class="dcg-form"&gt;
+	&lt;input class="dcg-input dcg-border-bottombar dcg-border-deep-red"&gt;
+&lt;/form&gt;
+</pre>
+
+					<div class="dcg-margin-top-large dcg-padding-top dcg-padding-bottom"><b>Background and Foreground</b></div>
+					<div class="dcg-padding-top dcg-padding-bottom">You can change the background or the foreground of a field using the color classes.<br><br><b>Demonstration of Dark Grey Background and Light Grey Foreground with Example Code</b></div>
+					<form class="dcg-form">
+						<input class="dcg-input dcg-dark-grey dcg-text-light-grey">
+					</form>
+<pre class="dcg-code">
+&lt;form class="dcg-form"&gt;
+	&lt;input class="dcg-input dcg-dark-grey dcg-text-light-grey"&gt;
+&lt;/form&gt;
+</pre>
+
+					<div class="dcg-margin-top-large dcg-padding-top dcg-padding-bottom">
+						<div class="dcg-large dcg-padding-bottom"><b>Labels</b></div>
+						The <b>dcg-label</b> class can be used to make a label sit nicely above the input field. It can be colored to match the field.
+						<br><br>
+						<b>Demonstration of a Label with Example Code</b>
+					</div>
+					<form class="dcg-form">
+						<label for="name" class="dcg-label dcg-text-deep-red">Name:</label>
+						<input id="name" class="dcg-input dcg-border-all dcg-border-deep-red dcg-text-deep-red">
+					</form>
+<pre class="dcg-code">
+&lt;form class="dcg-form"&gt;
+	&lt;label for="name" class="dcg-label dcg-text-deep-red"&gt;Name:&lt;/label&gt;
+	&lt;input id="name" class="dcg-input dcg-border-all dcg-border-deep-red dcg-text-deep-red"&gt;
+&lt;/form&gt;
+</pre>
+					<?php
 					break;
 			}
 			?>
